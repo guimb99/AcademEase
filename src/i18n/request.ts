b2082b72b690ffefa2e13797b5958a8dd3bf8e15
Guilherme B <guiboeira@yahoo.com.br>
2024-09-19 18:31:36 -0300

    default language to en-US

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,9 @@ import { getRequestConfig } from "next-intl/server";
 import { headers } from "next/headers";
 
 export default getRequestConfig(async () => {
-  const locale = headers().get("accept-language")?.split(',')[0];
+  const headerLocale = headers().get("accept-language")?.split(',')[0];
+  const supportedLanguages = ['en-US', 'pt-BR'];
+  const locale = supportedLanguages.includes(headerLocale!) ? headerLocale : 'en-US';
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
